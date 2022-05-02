f11d9ee7e00cafd1c3f51a3ca58efc467af73fef
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-05-02 18:32:16 +0200

    added censoredText

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php error_reporting (E_ALL ^ E_NOTICE); ?>
 <?php
 // Creare una variabile con un paragrafo di testo a vostra scelta.
 // Stampare a schermo il paragrafo e la sua lunghezza.
@@ -11,7 +12,14 @@ $textLenght = strlen($text);
 
 $bannedWord = $_GET['soccia'];
 
+$censoredText = str_replace($bannedWord, '***', $text);
+
+$censoredTextLenght = strlen($censoredText);
 ?>
 
 <h1>Original Text</h1>
 <h3><?php echo($text) ?></h3>
+<h4> <?php echo('Text lenght: ' . $textLenght) ?> </h4>
+
+<h1>Censored Text</h1>
+<h3><?php  ?></h3>
