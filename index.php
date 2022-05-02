2ac22ccfd4e12ca59fddbb6c12c32b26370b4870
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-05-02 17:57:24 +0200

    added variables and text in dom

--- index.php
+++ index.php
@@ -4,4 +4,14 @@
 // Una parola da censurare viene passata dall'utente tramite parametro GET.
 // Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
 
+
+$text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, assumenda!";
+
+$textLenght = strlen($text);
+
+$bannedWord = $_GET['soccia'];
+
 ?>
+
+<h1>Original Text</h1>
+<h3><?php echo($text) ?></h3>
