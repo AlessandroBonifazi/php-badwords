d55335adc3270bbc183e01d1c45d4ba1f270cb58
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-05-03 10:08:26 +0200

    indent changes

--- index.php
+++ index.php
@@ -5,24 +5,21 @@
 // Una parola da censurare viene passata dall'utente tramite parametro GET.
 // Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
 
-
 $text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, assumenda!";
 
 $textLenght = strlen($text);
 
-$bannedWord = $_GET["ohibo"];
+$bannedWord = $_GET["ban"];
 
+$censoredText = str_replace($bannedWord, '***', $text);
 
+$censoredTextLenght = strlen($censoredText);
 ?>
 
 <h1>Original Text</h1>
 <h3><?php echo($text) ?></h3>
 <h4> <?php echo('Text lenght: ' . $textLenght) ?> </h4>
-<?php 
-$censoredText = str_replace($bannedWord, '***', $text);
 
-$censoredTextLenght = strlen($censoredText);
-?>
 <h1>Censored Text</h1>
 <h3><?php echo($censoredText) ?></h3>
 <h4> <?php echo('Text lenght: ' . $censoredTextLenght) ?> </h4>
